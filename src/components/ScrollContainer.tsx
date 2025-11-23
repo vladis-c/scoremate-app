ba987585c3c524d834b9b0dfd1f1352d408c6338
Vladislav Cherkasheninov <vladcher26@yandex.ru>
2025-11-23 11:59:21 +0200

    fix: added keyboard avoiding view to scroll container

--- src/components/ScrollContainer.tsx
+++ src/components/ScrollContainer.tsx
@@ -1,5 +1,12 @@
 import React, {useEffect, useRef} from 'react';
-import {ScrollView, StyleProp, StyleSheet, ViewStyle} from 'react-native';
+import {
+  KeyboardAvoidingView,
+  Platform,
+  ScrollView,
+  StyleProp,
+  StyleSheet,
+  ViewStyle,
+} from 'react-native';
 import {useAppDispatch, useAppSelector} from '../hooks/redux-hooks';
 import {setShouldScrollToEnd} from '../store/service';
 
@@ -20,12 +27,17 @@ const ScrollContainer = ({children, style}: ScrollContainerProps) => {
   }, [shouldScrollToEnd]);
 
   return (
-    <ScrollView
-      style={{margin: 1, marginBottom: 90}}
-      contentContainerStyle={[styles.container, style]}
-      ref={ref}>
-      {children}
-    </ScrollView>
+    <KeyboardAvoidingView
+      style={{flex: 1}}
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+      keyboardVerticalOffset={30}>
+      <ScrollView
+        style={{margin: 1, marginBottom: 90}}
+        contentContainerStyle={[styles.container, style]}
+        ref={ref}>
+        {children}
+      </ScrollView>
+    </KeyboardAvoidingView>
   );
 };
 
