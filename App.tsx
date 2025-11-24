6b66f8cb2c99e43df5af8e1e79c929ab1c11ad4d
Vladislav Cherkasheninov <vladcher26@yandex.ru>
2025-11-24 12:25:11 +0200

    chore: removed redux

--- App.tsx
+++ App.tsx
@@ -6,12 +6,10 @@ import {ClickOutsideProvider} from 'react-native-click-outside';
 import 'react-native-gesture-handler';
 import {PaperProvider} from 'react-native-paper';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
-import {Provider} from 'react-redux';
 import {ScoreProvider} from './src/context/ScoreContext';
 import {useStart} from './src/hooks/useStart';
 import {useUpdate} from './src/hooks/useUpdate';
 import MainNavigator from './src/navigation/MainNavigator';
-import {store} from './src/store/store';
 import {NavigationTheme, PaperTheme, colors} from './src/theme';
 
 SplashScreen.preventAutoHideAsync();
@@ -37,15 +35,13 @@ const App = () => {
     <ScoreProvider>
       <SafeAreaProvider onLayout={hideSplashScreen}>
         <StatusBar backgroundColor={colors.LightBlue} animated={true} />
-        <Provider store={store}>
-          <PaperProvider theme={PaperTheme}>
-            <NavigationContainer theme={NavigationTheme}>
-              <ClickOutsideProvider>
-                <MainNavigator />
-              </ClickOutsideProvider>
-            </NavigationContainer>
-          </PaperProvider>
-        </Provider>
+        <PaperProvider theme={PaperTheme}>
+          <NavigationContainer theme={NavigationTheme}>
+            <ClickOutsideProvider>
+              <MainNavigator />
+            </ClickOutsideProvider>
+          </NavigationContainer>
+        </PaperProvider>
       </SafeAreaProvider>
     </ScoreProvider>
   );
